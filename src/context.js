b04456c7e5fc680809c51710044f42760e1428b0
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2018-05-20 13:44:43 +0200

    add context creation

---
+++ src/context.js
@@ -0,0 +1,6 @@
+import createReactContext from './tools/createContext';
+import { CONTEXT } from './defaults';
+
+const TranslationContext = createReactContext(CONTEXT);
+
+export default TranslationContext;
