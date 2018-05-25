8b8a8f5d39923f99e9a006ddef2b1a964160bb28
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2018-05-25 11:15:01 +0200

    support older react versions - add wrapper component for pre-16 string render

--- src/tools/safety.js
+++ src/tools/safety.js
@@ -4,7 +4,9 @@ const [M] = React.version.split('.');
 const major = parseInt(M, 10);
 const shouldWrapString = major < 16;
 
+const Wrapper = ({ children }) => <span>{children}</span>;
+
 const safelyRenderString = string =>
-  shouldWrapString ? <span>{string}</span> : string;
+  shouldWrapString ? <Wrapper>{string}</Wrapper> : string;
 
 export default safelyRenderString;
